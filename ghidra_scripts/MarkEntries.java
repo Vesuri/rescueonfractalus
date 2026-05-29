4d09487cef26479a127e09821596587fac9771c9
Vesa Halttunen <vesuri@jormas.com>
2026-05-29 19:48:38 +0200

    Add interrupt handler entry points to Ghidra analysis
    MarkEntries.java now marks all VBI handlers (vbi_handler_attract,
    vbi_handler_1/2/game at $1B30/4FF5/52D7/53CC), DLI handlers
    (dli_handler_game/game2 at $49EE/$6CC2) and the IRQ handler ($462A)
    as explicit entry points so Ghidra follows their control flow.
    
    This grew the disassembly from 225 functions/8K instructions to
    299 functions/11.7K instructions, adding all interrupt handlers.

--- ghidra_scripts/MarkEntries.java
+++ ghidra_scripts/MarkEntries.java
@@ -8,9 +8,23 @@ public class MarkEntries extends GhidraScript {
     @Override
     public void run() throws Exception {
         // addr, name  (game_entry is the final INITAD = $3CDE)
-        long[] addrs   = { 0x3CDEL, 0x3C00L, 0x5000L, 0x1B30L, 0x1A97L, 0xB800L };
-        String[] names = { "game_entry", "loader_3C00", "stage_5000",
-                           "code_1B30", "init_1A97", "init_B800" };
+        // Includes interrupt handlers (VBI/DLI/IRQ) not reached by normal JSR flow.
+        long[] addrs   = {
+            0x3CDEL, 0x3C00L, 0x5000L, 0x1B30L, 0x1A97L, 0xB800L,
+            /* VBI handlers (installed via VVBLKI $0222/3 OS shadow) */
+            0x53CCL, 0x4FF5L, 0x52D7L,
+            /* DLI handlers (installed via VDSLST $0200/1) */
+            0x49EEL, 0x6CC2L,
+            /* IRQ handler (installed via VIMIRQ $0216/7) */
+            0x462AL
+        };
+        String[] names = {
+            "game_entry", "loader_3C00", "stage_5000",
+            "vbi_handler_attract", "init_1A97", "init_B800",
+            "vbi_handler_1", "vbi_handler_2", "vbi_handler_game",
+            "dli_handler_game", "dli_handler_game2",
+            "irq_handler"
+        };
         for (int i = 0; i < addrs.length; i++) {
             Address a = toAddr(addrs[i]);
             addEntryPoint(a);
